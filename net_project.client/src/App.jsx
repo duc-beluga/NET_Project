336f9d2805144fa749194cd3155e25d5821f29b5
Duc Nguyen <dnguyen416@gatech.edu>
2024-03-25 15:31:23 -0400

    feat: added TailwindCSS (bug)

--- net_project.client/src/App.jsx
+++ net_project.client/src/App.jsx
@@ -1,48 +1,43 @@
 import { useEffect, useState } from 'react';
-import './App.css';
 
 function App() {
-    const [forecasts, setForecasts] = useState();
+    const [heroes, setHeroes] = useState([]);
 
     useEffect(() => {
-        populateWeatherData();
+        populateHeroData();
     }, []);
 
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
     return (
-        <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
+        <div className="flex justify-center items-center">
+            <div className="overflow-x-auto">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Place</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {heroes.map(hero => (<tr key={ hero.Name}>
+                            <td>{ hero.name}</td>
+                            <td>{ hero.firstName}</td>
+                            <td>{ hero.lastName}</td>
+                            <td>{ hero.place}</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
     
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
+    async function populateHeroData() {
+        const response = await fetch('api/SuperHero');
         const data = await response.json();
-        setForecasts(data);
+        setHeroes(data);
+        console.log(data)
     }
 }
 
